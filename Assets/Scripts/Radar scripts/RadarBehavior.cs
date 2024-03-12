f38e8ebab174ef4d61d857064b95b9f4547a73be
Oskar B. Kaare <oskar.kaare@gmail.com>
2024-03-12 11:39:06 +0100

    did some fuckiwocki

--- Assets/Scripts/Radar scripts/RadarBehavior.cs
+++ Assets/Scripts/Radar scripts/RadarBehavior.cs
@@ -29,9 +29,9 @@ public class RadarBehavior : MonoBehaviour
         {
             randomItem = Random.Range(0, spawnPool.Count); // here we chose one random item from the spawnpool list
             toSpawn = spawnPool[randomItem]; // that random chosen gameobject is now set to "toSpawn"
-            screenX = Random.Range(radar.bounds.min.x, radar.bounds.max.x);// limits the area where the gameobjects can spawn on the x axis to be inside the bounds of the circle   
-            screenY = Random.Range(radar.bounds.min.y, radar.bounds.max.y);// limits the area where the gameobjects can spawn on the y axis to be inside the bounds of the  circle
-            screenZ = Random.Range(radar.bounds.min.z, radar.bounds.max.z);// limits the area where the gameobjects can spawn on the z axis to be inside the bounds of the circle
+            screenX = Random.Range(radar.bounds.min.x, radar.bounds.max.x);// limits the area where the gameobjects can spawn on the x axis to be inside the bounds of the sphere 
+            screenY = Random.Range(radar.bounds.min.y, radar.bounds.max.y);// limits the area where the gameobjects can spawn on the y axis to be inside the bounds of the  sphere
+            screenZ = Random.Range(radar.bounds.min.z, radar.bounds.max.z);// limits the area where the gameobjects can spawn on the z axis to be inside the bounds of the sphere
             pos = new Vector3(screenX, screenY, screenZ); // pos now gets values, which is a new vector3, that contains the xyz map bounds 
 
 
@@ -39,5 +39,5 @@ public class RadarBehavior : MonoBehaviour
             Instantiate(toSpawn, pos, toSpawn.transform.rotation); // we instantiate the chosen prop, with the new vector3 positition with a default rotation
         }
 
-    }
+    } //important to credit the same guy from the p1 project for helping me with this script
 }
