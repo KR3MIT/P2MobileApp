314830a629b9032c82e443a1685e9845c74e2e18
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-03-13 13:55:31 +0100

    Test time

--- Assets/Scripts/LocationManager.cs
+++ Assets/Scripts/LocationManager.cs
@@ -38,48 +38,47 @@ public class LocationManager : MonoBehaviour
     private IEnumerator AskForLocation()
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
-        // Android-specific code
-        if (!Input.location.isEnabledByUser)
-        {
-            Debug.Log("Location services are not enabled");
-            yield break;
-        }
+    // Android-specific code
+    if (!Input.location.isEnabledByUser)
+    {
+        Debug.Log("Location services are not enabled");
+        UseDefaultLocation();
+        yield break;
+    }
 
-        Input.location.Start();
+    Input.location.Start();
 
-        int maxWait = 20;
-        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
-        {
-            yield return new WaitForSeconds(1);
-            maxWait--;
-        }
+    int maxWait = 20;
+    while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+    {
+        yield return new WaitForSeconds(1);
+        maxWait--;
+    }
 
-        if (maxWait < 1)
-        {
-            Debug.Log("Timed out");
-            yield break;
-        }
+    if (maxWait < 1)
+    {
+        Debug.Log("Timed out");
+        UseDefaultLocation();
+        yield break;
+    }
 
-        if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            Debug.Log("Unable to determine device location");
-            yield break;
-        }
-        else
-        {
-            permission = true;
-            isLocationEnabled = true;
-            currentLocation = Input.location.lastData;
-            userLongitude = currentLocation.longitude;
-            userLatitude = currentLocation.latitude;
-        }
-#else
-        // Use default or simulated location for non-Android platforms and Unity Editor
-        Debug.Log("Using default location");
+    if (Input.location.status == LocationServiceStatus.Failed)
+    {
+        Debug.Log("Unable to determine device location");
+        UseDefaultLocation();
+        yield break;
+    }
+    else
+    {
         permission = true;
         isLocationEnabled = true;
-        userLongitude = defaultLongitude;
-        userLatitude = defaultLatitude;
+        currentLocation = Input.location.lastData;
+        userLongitude = currentLocation.longitude;
+        userLatitude = currentLocation.latitude;
+    }
+#else
+        // Use default or simulated location for non-Android platforms and Unity Editor
+        UseDefaultLocation();
 #endif
 
         // Common code for calculating bounding box, runs after platform-specific location retrieval
@@ -90,5 +89,14 @@ public class LocationManager : MonoBehaviour
         map.BoundingBox[2] = userLongitude + deltaLongitude;
         map.BoundingBox[3] = userLatitude + deltaLatitude;
         yield break;
+    }
+
+    private void UseDefaultLocation()
+    {
+        Debug.Log("Using default location");
+        permission = true;
+        isLocationEnabled = true;
+        userLongitude = defaultLongitude;
+        userLatitude = defaultLatitude;
     }
 }
