af40306a5a5f2db8b370143a90aa52289c255405
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-03-21 14:29:23 +0100

    fuck af

--- Assets/Scripts/Character/CharacterScene.cs
+++ Assets/Scripts/Character/CharacterScene.cs
@@ -1,35 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class CharacterScene : MonoBehaviour
-{
-    [Tooltip("Scenes in which the players visuals will be displayed")]
-    [SerializeField] private List<SceneAsset> SceneAssets = new List<SceneAsset>();
-    private List<string> _scenes = new List<string>();
-    [SerializeField] private GameObject playerVisuals;
+//public class CharacterScene : MonoBehaviour
+//{
+//    [Tooltip("Scenes in which the players visuals will be displayed")]
+//    [SerializeField] private List<SceneAsset> SceneAssets = new List<SceneAsset>();
+//    private List<string> _scenes = new List<string>();
+//    [SerializeField] private GameObject playerVisuals;
 
     
-    void Start()
-    {
-        SceneManager.activeSceneChanged += ChangedActiveScene;
+//    void Start()
+//    {
+//        SceneManager.activeSceneChanged += ChangedActiveScene;
 
-        foreach(SceneAsset obj in SceneAssets)
-        {
-            _scenes.Add(obj.name);
-        }
-    }
+//        foreach(SceneAsset obj in SceneAssets)
+//        {
+//            _scenes.Add(obj.name);
+//        }
+//    }
 
-    private void ChangedActiveScene(Scene currentScene, Scene nextScene)
-    {
-        Debug.Log($"CurrentScene: {currentScene.name} NextScene: {nextScene.name}");
+//    private void ChangedActiveScene(Scene currentScene, Scene nextScene)
+//    {
+//        Debug.Log($"CurrentScene: {currentScene.name} NextScene: {nextScene.name}");
 
-        if (_scenes.Contains(nextScene.name))
-        {
-            playerVisuals.SetActive(true);
-        }
-        else { playerVisuals.SetActive(false); }
-    }
-}
+//        if (_scenes.Contains(nextScene.name))
+//        {
+//            playerVisuals.SetActive(true);
+//        }
+//        else { playerVisuals.SetActive(false); }
+//    }
+//}
