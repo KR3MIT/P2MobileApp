0a7b87b8e3d54b5b1cd16a728caa664986ab6c5f
m4tt <33161422+m4ttxdd@users.noreply.github.com>
2024-03-25 10:10:12 +0100

    dada

--- Assets/Scripts/Character/CharacterScene.cs
+++ Assets/Scripts/Character/CharacterScene.cs
@@ -1,35 +1,35 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEditor;
-//using UnityEngine;
-//using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
-//public class CharacterScene : MonoBehaviour
-//{
-//    [Tooltip("Scenes in which the players visuals will be displayed")]
-//    [SerializeField] private List<SceneAsset> SceneAssets = new List<SceneAsset>();
-//    private List<string> _scenes = new List<string>();
-//    [SerializeField] private GameObject playerVisuals;
+public class CharacterScene : MonoBehaviour
+{
+    [Tooltip("Scenes in which the players visuals will be displayed")]
+    [SerializeField] private List<SceneAsset> SceneAssets = new List<SceneAsset>();
+    private List<string> _scenes = new List<string>();
+    [SerializeField] private GameObject playerVisuals;
 
-    
-//    void Start()
-//    {
-//        SceneManager.activeSceneChanged += ChangedActiveScene;
 
-//        foreach(SceneAsset obj in SceneAssets)
-//        {
-//            _scenes.Add(obj.name);
-//        }
-//    }
+    void Start()
+    {
+        SceneManager.activeSceneChanged += ChangedActiveScene;
 
-//    private void ChangedActiveScene(Scene currentScene, Scene nextScene)
-//    {
-//        Debug.Log($"CurrentScene: {currentScene.name} NextScene: {nextScene.name}");
+        foreach (SceneAsset obj in SceneAssets)
+        {
+            _scenes.Add(obj.name);
+        }
+    }
 
-//        if (_scenes.Contains(nextScene.name))
-//        {
-//            playerVisuals.SetActive(true);
-//        }
-//        else { playerVisuals.SetActive(false); }
-//    }
-//}
+    private void ChangedActiveScene(Scene currentScene, Scene nextScene)
+    {
+        Debug.Log($"CurrentScene: {currentScene.name} NextScene: {nextScene.name}");
+
+        if (_scenes.Contains(nextScene.name))
+        {
+            playerVisuals.SetActive(true);
+        }
+        else { playerVisuals.SetActive(false); }
+    }
+}
