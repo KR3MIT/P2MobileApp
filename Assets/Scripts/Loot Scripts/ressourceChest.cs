caa323085084ab6c8b738b058a62ffd682a41fb3
Oskar B. Kaare <oskar.kaare@gmail.com>
2024-03-25 12:20:36 +0100

    emitter

--- Assets/Scripts/Loot Scripts/ressourceChest.cs
+++ Assets/Scripts/Loot Scripts/ressourceChest.cs
@@ -8,8 +8,8 @@ public class ressourceChest : MonoBehaviour
 {
     private Character character;
     public TMPro.TextMeshProUGUI resourceGainText;
+    public ParticleSystem winParticle;
 
-  
     private Dictionary<ResourceType, int> resourcesToGive = new Dictionary<ResourceType, int>
     {
         {ResourceType.Wood, 1},
@@ -29,7 +29,9 @@ public class ressourceChest : MonoBehaviour
         if(character != null)
         {
             OpenChestResource();
-        }else Debug.Log("Character not found");
+            winParticle.Play();
+        }
+        else Debug.Log("Character not found");
         
 
     }
@@ -45,6 +47,9 @@ public class ressourceChest : MonoBehaviour
             Debug.Log("wood amount: " + resourcesToGive[ResourceType.Wood]);
             Debug.Log(random);
 
+            ParticleSystem.MainModule settings = winParticle.main;
+            settings.startColor = new ParticleSystem.MinMaxGradient(Color.green);
+
             character.AddResource(ResourceType.Wood, resourcesToGive[ResourceType.Wood]);
             resourceGainText.text = "You found " + resourcesToGive[ResourceType.Wood] + " " + "Wood";
 
@@ -56,6 +61,9 @@ public class ressourceChest : MonoBehaviour
             Debug.Log("Scrap Metal amount: " + resourcesToGive[ResourceType.Metal]);
             Debug.Log(random);
 
+            ParticleSystem.MainModule settings = winParticle.main;
+            settings.startColor = new ParticleSystem.MinMaxGradient(Color.grey);
+
             character.AddResource(ResourceType.Metal, resourcesToGive[ResourceType.Metal]);
             resourceGainText.text = "You found " + resourcesToGive[ResourceType.Metal] + " " + "Scrap Metal";
         }
@@ -66,6 +74,9 @@ public class ressourceChest : MonoBehaviour
             Debug.Log("gold amount: " + resourcesToGive[ResourceType.Gold]);
             Debug.Log(random);
 
+            ParticleSystem.MainModule settings = winParticle.main;
+            settings.startColor = new ParticleSystem.MinMaxGradient(Color.yellow);
+
             character.AddResource(ResourceType.Gold, resourcesToGive[ResourceType.Gold]);
             resourceGainText.text = "You found " + resourcesToGive[ResourceType.Gold] + " " + "Gold Ingot";
         }
@@ -75,6 +86,9 @@ public class ressourceChest : MonoBehaviour
             resourcesToGive[ResourceType.Diamonds] = Random.Range(1, 4);
             Debug.Log("diamond amount: " + resourcesToGive[ResourceType.Diamonds]);
             Debug.Log(random);
+
+            ParticleSystem.MainModule settings = winParticle.main;
+            settings.startColor = new ParticleSystem.MinMaxGradient(Color.cyan);
 
             character.AddResource(ResourceType.Diamonds, resourcesToGive[ResourceType.Diamonds]);
             resourceGainText.text = "You found " + resourcesToGive[ResourceType.Diamonds] + " " + "Shiny Diamond";
