82042d059b62ccf5884e7b322136023bfdfbaf31
Oskar B. Kaare <oskar.kaare@gmail.com>
2024-04-02 12:09:21 +0200

    embark stuff

--- Assets/Scripts/Radar scripts/EmbarkAllower.cs
+++ Assets/Scripts/Radar scripts/EmbarkAllower.cs
@@ -1,14 +1,23 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EmbarkAllower : MonoBehaviour
-{
+{   
     private bool embarkAllowed = false;
     DateTime currentDate;
     DateTime previousDate;
- 
+
+    private void Start()
+    {
+        AllowEmbark();
+        Debug.Log(PlayerPrefs.GetString("previousDate"));
+        Debug.Log(currentDate);
+        AllowEmbark();
+
+    }
     //if date change, embarkallowed = true
     public void AllowEmbark()
     {
@@ -26,12 +35,14 @@ public class EmbarkAllower : MonoBehaviour
 ;       Embark();
         previousDate = currentDate;
         embarkAllowed = false;
+        PlayerPrefs.SetString("previousDate", previousDate.ToString());
     }
     void Embark()
     {
         // start the scan from other script
         //load map
         //load player??
+        
         Debug.Log("Embarking");
     }
    
