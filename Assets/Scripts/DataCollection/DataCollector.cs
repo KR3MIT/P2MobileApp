4a52a84d3b352f5b07662e60ed0934ee9db91137
William Abildtrup <william@abildtrup.eu>
2024-04-03 10:08:44 +0200

    dunno

--- Assets/Scripts/DataCollection/DataCollector.cs
+++ Assets/Scripts/DataCollection/DataCollector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Android;
 using UnityEngine.InputSystem;
 
 public class DataCollector : MonoBehaviour
@@ -15,17 +16,25 @@ public class DataCollector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        
+
     }
         
     // Update is called once per frame
     void Update()
     {
-        
+        currentSteps = StepCounter.current.stepCounter.ReadValue();
+        Debug.Log("Number of steps: " + currentSteps);
+
+
     }
 
-    private void OnEnable()
+    private void OnEnable() // Runs when the Gameobject this script is on is enabled (when the game starts)
     {
+        if (!Permission.HasUserAuthorizedPermission("android.permission.ACTIVITY_RECOGNITION"))
+        {
+            Permission.RequestUserPermission("android.permission.ACTIVITY_RECOGNITION");
+        }
         // Enable the step counter
         InputSystem.AddDevice<StepCounter>();
         if (StepCounter.current == null)
@@ -40,7 +49,7 @@ public class DataCollector : MonoBehaviour
         Debug.Log("Number of steps: " + currentSteps);
     }
 
-    private void OnDisable()
+    private void OnDisable() // Runs when the Gameobject this script is on is disabled
     {
         // Disable the step counter
         InputSystem.DisableDevice(StepCounter.current);
