b7fb11c859d602131de6a3242684ac9fc6fc7306
William Abildtrup <william@abildtrup.eu>
2024-04-04 09:35:23 +0200

    prefabs to UI

--- Assets/Scripts/DataCollection/Analytics.cs
+++ Assets/Scripts/DataCollection/Analytics.cs
@@ -7,15 +7,15 @@ using Unity.Services.Analytics;
 // This class is responsible for initializing the Unity Services SDK and starting data collection.
 public class Analytics : MonoBehaviour
 {
-    [SerializeField] private bool consentGiven = false; // A boolean to track whether the player has given consent to collect data.
     public GameObject consentUIPrefab; // A reference to a UI element that asks for consent.
-    public int _consentGiven = 0; 
+    public int _consentGiven; 
    
 
     async void Start()
     {
         await UnityServices.InitializeAsync(); // Initialize the Unity Services SDK.
-       PlayerPrefs.GetInt("ConsentGiven", _consentGiven);
+        _consentGiven = PlayerPrefs.GetInt("ConsentGiven");
+        Debug.Log("Consentgiven on start: " + _consentGiven);
         if (_consentGiven == 0)
         {
             AskForConsent(); // Ask the player for consent to collect data.
@@ -38,12 +38,15 @@ public class Analytics : MonoBehaviour
         AnalyticsService.Instance.StartDataCollection();
         _consentGiven = 1;
         PlayerPrefs.SetInt("ConsentGiven", _consentGiven);
+        Debug.Log("Consentgiven after click: " + _consentGiven);
         consentUIPrefab.SetActive(false);
     }
 
     private void ConsentDenied()
     {
         Debug.Log("Consent denied. Data collection will not start.");
+        _consentGiven = 0;
+        PlayerPrefs.SetInt("ConsentGiven", _consentGiven);
         consentUIPrefab.SetActive(false);
     }
     private void RemoveConsent()
