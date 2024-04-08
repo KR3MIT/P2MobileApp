0e677ceb045ce652a79d6e6395bc942c1b5947d3
William Abildtrup <william@abildtrup.eu>
2024-04-08 13:29:44 +0200

    enemyscalingscript

--- Assets/Scripts/CombatEncounterTest/EnemyCharacterTest.cs
+++ Assets/Scripts/CombatEncounterTest/EnemyCharacterTest.cs
@@ -1,12 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 // This script was developed with the help of Github Co-pilot.
 
 public class EnemyCharacterTest : MonoBehaviour
 {
+    public int level;
+    public float health;
     public int defensePower;
     public int attackPower;
-    public float health;
+
+    public void ScaleLevel(int lvl)
+    {
+        health = 93 + (lvl * 7);
+        defensePower = 3 + (lvl * 7);
+        attackPower = 3 + (lvl * 7);
+    }
+
+    public void RandomizeLevel(int lvl)
+    {
+       
+        level = Random.Range(-2,lvl+2);
+        if (level < 1)
+        {
+            level = 1;
+        }
+        ScaleLevel(level);
+    }
+
 }
