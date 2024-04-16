c47c269eb27132231a2564cd1c34067808e76922
KR3MIT <jason.cm.hassan@gmail.com>
2024-04-16 09:56:08 +0200

    smol ui acc changes

--- Assets/AccountGUI.cs
+++ Assets/AccountGUI.cs
@@ -6,68 +6,63 @@ public class AccountGUI : MonoBehaviour
 {
     [SerializeField] private TMP_Text reqText;
     [SerializeField] private GameObject reqPanel;
+    [SerializeField] private GameObject activePos;
 
-    [Header("User Panel")]
     [SerializeField] private GameObject userPanel;
-    [SerializeField] private int userDefaultPos = 0;
-    [SerializeField] private int userActivePos = 0;
+    private Vector3 userDefaultPos;
     private TMP_InputField userInput;
 
-    [Header("Password Panel")]
     [SerializeField] private GameObject passPanel;
-    [SerializeField] private int passDefaultPos = 0;
-    [SerializeField] private int passActivePos = 0;
+    private Vector3 passDefaultPos;
     private TMP_InputField passInput;
 
-    // Start is called before the first frame update
     void Start()
-    { 
+    {
         reqPanel.SetActive(false);
 
         userInput = userPanel.GetComponent<TMP_InputField>();
         passInput = passPanel.GetComponent<TMP_InputField>();
 
-        userPanel.transform.position = new Vector3(userPanel.transform.position.x, userDefaultPos, userPanel.transform.position.z);
-        passPanel.transform.position = new Vector3(passPanel.transform.position.x, passDefaultPos, passPanel.transform.position.z);
-
+        userDefaultPos = userPanel.transform.position;
+        passDefaultPos = passPanel.transform.position;
+        
         //call method userinputactive when inputfield is active or pressed
         userInput.onSelect.AddListener(delegate { userInputActive(); });
         userInput.onDeselect.AddListener(delegate { userInputDeactive(); });
-
+        //pass
         passInput.onSelect.AddListener(delegate { passInputActive(); });
         passInput.onDeselect.AddListener(delegate { passInputDeactive(); });
-
     }
-    // this method is called when the user clicks on the input field
+    //user panel toggle
+    #region(user panel toggle)
     public void userInputActive()
     {
         Debug.Log("Balls User Active"); 
-        userPanel.transform.position = new Vector3(userPanel.transform.position.x, userActivePos, userPanel.transform.position.z);
-
+        userPanel.transform.position = activePos.transform.position;
         reqPanel.SetActive(true);
-        reqText.text = "Must <b>not</b> contain: \n<b>Spaces</b> or more then <b>30</b> characters";
-
+        reqText.text = "Must <b>not</b> contain: \n<b>Spaces</b> or more then <b>30</b> characters.";
     }
     public void userInputDeactive()
     {
         Debug.Log("Balls User Deactive");
-        userPanel.transform.position = new Vector3(userPanel.transform.position.x, userDefaultPos, userPanel.transform.position.z);
-
+        userPanel.transform.position = userDefaultPos;
         reqPanel.SetActive(false);
     }
+    #endregion
+    //password panel toggle
+    #region(password panel toggle)
     public void passInputActive()
     {
         Debug.Log("Balls Pass Active");
-        passInput.transform.position = new Vector3(passInput.transform.position.x, passActivePos, passInput.transform.position.z);
-
+        passPanel.transform.position = activePos.transform.position;
         reqPanel.SetActive(true);
         reqText.text = "Case sensitive, min of <b>8</b> and max of <b>30</b> characters, at least <b>1</b> uppercase, <b>1</b> lowercase, <b>1</b> number and <b>1</b> symbol.";
     }
     public void passInputDeactive()
     {
         Debug.Log("Balls Pass Deactive");
-        passInput.transform.position = new Vector3(passInput.transform.position.x, passDefaultPos, passInput.transform.position.z);
-
+        passPanel.transform.position = passDefaultPos;
         reqPanel.SetActive(false);
     }
+    #endregion
 }
