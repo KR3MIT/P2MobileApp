b2684cf8bf6fb674ab29940dcd0fbf7b5da4d803
Oskar B. Kaare <oskar.kaare@gmail.com>
2024-05-06 10:28:21 +0200

    Update ressourceChest.cs

--- Assets/Scripts/Loot Scripts/ressourceChest.cs
+++ Assets/Scripts/Loot Scripts/ressourceChest.cs
@@ -4,7 +4,12 @@ using Unity.Collections.LowLevel.Unsafe;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
+// this is a singleton class that holds the resources that the player can get from the chest
 using static Character;// so i doint have to write Character.ResourceType every time
+
+//
+// this script is created with the help of Github Copilot
+//
 
 public class ressourceChest : MonoBehaviour
 {
@@ -14,8 +19,14 @@ public class ressourceChest : MonoBehaviour
     [SerializeField] private GameObject panel;
     [SerializeField] private Sprite wood,metal,gold,diamond; 
     private ParticleSystem particleSystem;
-    
 
+    private float soundsDelay = 1.5f;
+    private float textDelay = 0.5f;
+
+    private int lowerAmount = 15;
+    private int upperAmount = 30;
+    
+    // this is a dictionary that holds the resources that the player can get from the chest and the amount of the resource is default 1
     private Dictionary<ResourceType, int> resourcesToGive = new Dictionary<ResourceType, int>
     {
         {ResourceType.Wood, 1},
@@ -31,6 +42,7 @@ public class ressourceChest : MonoBehaviour
         character = FindObjectOfType<Character>();
         particleSystem = winParticle.GetComponent<ParticleSystem>();
     }
+    //this method is called when a chest is clicked and if the character singleton is there it calls the OpenChestResource method it plays the particle system 
     public void OpenChest(string ChestClicked)
     {
         if(character != null)
@@ -46,17 +58,18 @@ public class ressourceChest : MonoBehaviour
     //Method to open the chest and get a random resource with a random amount
     public void OpenChestResource()
     {
+        // random is set to a random number between 0 and 100
         int random = Random.Range(0, 100);
         if (random < 25)
         {
             Debug.Log("Wood");
-            resourcesToGive[ResourceType.Wood] = Random.Range(15, 30);
+            resourcesToGive[ResourceType.Wood] = Random.Range(lowerAmount, upperAmount);
             Debug.Log("wood amount: " + resourcesToGive[ResourceType.Wood]);
             Debug.Log(random);
           
           particleSystem.textureSheetAnimation.AddSprite(wood);
             
-
+            // this adds the resource to the character singleton and then calls the TextDelay method to display the resource gain text
             character.AddResource(ResourceType.Wood, resourcesToGive[ResourceType.Wood]);
             StartCoroutine(TextDelay("Wood", resourcesToGive[ResourceType.Wood]));
             
@@ -65,37 +78,37 @@ public class ressourceChest : MonoBehaviour
         else if (random < 50)
         {
             Debug.Log("Scrap Metal");
-            resourcesToGive[ResourceType.Metal] = Random.Range(15, 30);
+            resourcesToGive[ResourceType.Metal] = Random.Range(lowerAmount, upperAmount);
             Debug.Log("Scrap Metal amount: " + resourcesToGive[ResourceType.Metal]);
             Debug.Log(random);
 
             particleSystem.textureSheetAnimation.AddSprite(metal);
-           
 
+            // this adds the resource to the character singleton and then calls the TextDelay method to display the resource gain text
             character.AddResource(ResourceType.Metal, resourcesToGive[ResourceType.Metal]);
             StartCoroutine(TextDelay("Scrap Metal", resourcesToGive[ResourceType.Metal]));
         }
         else if (random < 75)
         {
             Debug.Log("Gold Ingot");
-            resourcesToGive[ResourceType.Gold] = Random.Range(15, 30);
+            resourcesToGive[ResourceType.Gold] = Random.Range(lowerAmount, upperAmount);
             Debug.Log("gold amount: " + resourcesToGive[ResourceType.Gold]);
             Debug.Log(random);
 
             particleSystem.textureSheetAnimation.AddSprite(gold);
-
+            // this adds the resource to the character singleton and then calls the TextDelay method to display the resource gain text
             character.AddResource(ResourceType.Gold, resourcesToGive[ResourceType.Gold]);
             StartCoroutine(TextDelay("Gold Ingots", resourcesToGive[ResourceType.Gold]));
         }
         else
         {
             Debug.Log("Shiny Diamond"); 
-            resourcesToGive[ResourceType.Diamonds] = Random.Range(15, 30);
+            resourcesToGive[ResourceType.Diamonds] = Random.Range(lowerAmount, upperAmount);
             Debug.Log("diamond amount: " + resourcesToGive[ResourceType.Diamonds]);
             Debug.Log(random);
 
             particleSystem.textureSheetAnimation.AddSprite(diamond);
-
+            // this adds the resource to the character singleton and then calls the TextDelay method to display the resource gain text
             character.AddResource(ResourceType.Diamonds, resourcesToGive[ResourceType.Diamonds]);
             StartCoroutine(TextDelay("Shiny Diamonds", resourcesToGive[ResourceType.Diamonds]));
           
@@ -104,9 +117,10 @@ public class ressourceChest : MonoBehaviour
         
         IEnumerator TextDelay(string stringType, int resourceToGive)
         {
-            yield return new WaitForSeconds(1.5f);
+            // this coroutine waits for 1.5 seconds and then plays the open chest sound and then waits for 0.5 seconds and then displays the resource gain text
+            yield return new WaitForSeconds(soundsDelay);
             AudioManager.instance.Play("chestSound");
-            yield return new WaitForSeconds(0.5f);
+            yield return new WaitForSeconds(textDelay);
             resourceGainText.text = resourceToGive + "x " + stringType;
             panel.SetActive(true);
           
